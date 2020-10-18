5ab718ce13399d981ab4e979fa1f845673a42b58
Peter <boys4849@gmail.com>
2020-10-18 16:12:13 +0200

    final

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -2,6 +2,7 @@
 function displayRadioValue() {
 
         const ele = document.getElementsByName('decision');
+        document.getElementById("success").style.display = 'none';
         document.getElementById('country').style.display = 'none';
         document.getElementById('scenes').style.display = 'none';
         document.getElementById("height_block").style.display = "none";
@@ -10,7 +11,8 @@ function displayRadioValue() {
         document.getElementById('custom_value').style.display = 'none';
         document.getElementById('position').style.display = 'none';
         document.getElementById('textarea-block').style.display = "none";
-
+        document.getElementById('custom_value3').style.display = "none";
+         document.getElementById('custom_value2').style.display = 'none';
         for(let i = 0; i < ele.length; i++) {
             if(ele[i].checked){
                 if(ele[i].value === 'Yes'){
@@ -27,20 +29,27 @@ function displaySelectValue() {
     const ele = document.getElementById('select_project').value;
     document.getElementById('custom_value').style.display = 'none';
     document.getElementById('position').style.display = 'none';
+    document.getElementById('textarea-block').style.display = "none";
+    document.getElementById('custom_value3').style.display = "none";
+    document.getElementById("success").style.display = 'none';
+    document.getElementById('scenes').style.display = 'none';
+    document.getElementById('country').style.display = 'none';
+    document.getElementById('custom_value2').style.display = 'none';
     if(ele==='Komparz'){
-        window.alert(ele);
+        document.getElementById('custom_value3').style.display = "inline";
     }if(ele==='Filmový herec'){
         document.getElementById('position').style.display = 'inline';
     }if(ele==='Seriálový herec'){
         document.getElementById('position').style.display = 'inline';
     }if(ele==='Účinkoval v relácii'){
-        window.alert(ele);
+        document.getElementById('custom_value3').style.display = "inline";
     }if(ele==='Zábavný program'){
-        window.alert(ele);
+        document.getElementById('custom_value3').style.display = "inline";
+        document.getElementById("success").style.display = 'inline';
     }if(ele==='Vlastné'){
         document.getElementById('custom_value').style.display = 'inline';
     }if(ele==='Nebol'){
-        window.alert(ele);
+        document.getElementById('textarea-block').style.display = "inline";
     }
 
 }
@@ -48,6 +57,7 @@ function displaySelectValue2() {
     const ele = document.getElementById('select_position').value;
     document.getElementById('country').style.display = 'none';
     document.getElementById('scenes').style.display = 'none';
+    document.getElementById('custom_value2').style.display = 'none';
     if(ele==="Hlavná rola"){
         document.getElementById('country').style.display = 'inline';
 
@@ -90,29 +100,16 @@ function validateForm(){
         alert("Dátum narodenia musí byť vyplnený");
         return false;
     }
-    const age=document.forms["form_actors"]["age"].value;
-    let currentDate = new Date(); //17.10.2020
-    const year = new Date(document.getElementById("birthDate").value).getFullYear();//1999
-    const month = new Date(document.getElementById("birthDate").value).getMonth();//08
-    const day = new Date(document.getElementById("birthDate").value).getDate();//11
-    let ageMy = currentDate.getFullYear() - year;
-    if(currentDate.getMonth() - month < 0){
-        ageMy= ageMy-1;
-    }
-    if(currentDate.getMonth() === month){
-        if(currentDate.getDate()- day < 0){
-            ageMy= ageMy-1;
-        }
-    }
-    if(ageMy!=age){
-        alert("Zlý vek\n"+"\n"+"Poďla narodenia "+ageMy+"\n"+"Podľa vstupu "+age);
-        return false;
-    }
+
+
+    validateAge();
+    validateEmail();
 
 
 
-
-    x = document.forms["form_actors"]["email"].value;
+}
+function validateEmail(){
+    let x = document.forms["form_actors"]["email"].value;
     const res = x.split("@", 1);
     res[0]=res[0].length;
     if(res < 3){
@@ -120,8 +117,42 @@ function validateForm(){
         return false;
     }
 
+    const dom = x.split(".");
+    let domLength = dom.length;
+    if(dom.length<3){
+        alert("Musia byť aspoň dve domény.");
+        return false;
+    }
+    for (let i = 1; i < domLength;i++) {
+        if(dom[i].length < 2||dom[i].length > 4){
+            alert("Domény musia obsahovať aspoň 2 znaky a max 4.");
+            return false;}
+    }
+}
 
+function validateAge(){
 
+    const age=document.forms["form_actors"]["age"].value;
+    let currentDate = new Date(); //17.10.2020
+    const year = new Date(document.getElementById("birthDate").value).getFullYear();//1999
+    const month = new Date(document.getElementById("birthDate").value).getMonth();//08
+    const day = new Date(document.getElementById("birthDate").value).getDate();//11
+    let ageMy = currentDate.getFullYear() - year;
+    if(age === ""){
+        return true;
+    }
+    if(currentDate.getMonth() - month < 0){
+        ageMy= ageMy-1;
+    }
+    if(currentDate.getMonth() === month){
+        if(currentDate.getDate() - day < 0){
+            ageMy= ageMy-1;
+        }
+    }
+    if(ageMy!=age){
+        alert("Zlý vek\n"+"\n"+"Poďla narodenia "+ageMy+"\n"+"Podľa vstupu "+age);
+        return false;
+    }
 }
 
 let select = document.getElementById("select_project");
